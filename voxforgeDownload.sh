a744cb9decb681616c7904a6be06df441fef67f9
Ubuntu <ubuntu@ip-172-31-53-76.ec2.internal>
2017-03-09 21:25:29 +0000

    modify downloading script

--- voxforgeDownload.sh
+++ voxforgeDownload.sh
@@ -2,9 +2,9 @@ DATA_SRC="http://www.repository.voxforge1.org/downloads/SpeechCorpus/Trunk/Audio
 DATA_ROOT="/home/ubuntu/voxforge"
 DATA_TGZ=${DATA_ROOT}/tgz
 DATA_EXTRACT=${DATA_ROOT}/audio
+mkdir ${DATA_EXTRACT}
 
-wget -P ${DATA_TGZ} -l 1 -N -nd -c -e robots=off -A tgz -r -np ${DATA_SRC} || \
- { echo "WGET error"'!' ; exit 1 ; }
+wget -P ${DATA_TGZ} -l 1 -N -nd -c -e robots=off -A tgz -r -np ${DATA_SRC}
 
 for a in ${DATA_TGZ}/*.tgz; do
   tar -C ${DATA_EXTRACT} -xf $a
