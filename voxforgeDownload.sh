08326bfaab3570ffbc11089c5826e0fbf04971c3
Ubuntu <ubuntu@ip-172-31-58-93.ec2.internal>
2017-03-10 01:10:07 +0000

    MFCC generated

--- voxforgeDownload.sh
+++ voxforgeDownload.sh
@@ -2,6 +2,7 @@ DATA_SRC="http://www.repository.voxforge1.org/downloads/SpeechCorpus/Trunk/Audio
 DATA_ROOT="/home/ubuntu/voxforge"
 DATA_TGZ=${DATA_ROOT}/tgz
 DATA_EXTRACT=${DATA_ROOT}/audio
+
 mkdir ${DATA_EXTRACT}
 
 wget -P ${DATA_TGZ} -l 1 -N -nd -c -e robots=off -A tgz -r -np ${DATA_SRC}
